712290fb1c66fccbf20b4ff27c85e25fd80b379f
Marc Sanchez <163992267+Saikurukaizen@users.noreply.github.com>
2025-03-05 10:23:04 +0100

    Add files via upload

--- src/calificaciones.php
+++ src/calificaciones.php
@@ -1,20 +1,21 @@
 <?php
 
+namespace app;
+
 class Calificaciones{
-    public function Calificacion($nota){
-        if($nota >= 0.60){
-            return 'Ets Primera Divisió';
+
+    private $nota;
+
+    public function __construct($nota){
+        $this->nota = $nota;
+    }
+    public function Aprobado($nota, $notaMaxima){
+        if($nota > 0 || $nota > $notaMaxima){
+            return false;
         }
-        elseif($nota >= 0.45 && $nota < 0.60){
-            return 'Ets Segona Divisió';
-        }
-        elseif($nota >= 0.33 && $nota < 0.45){
-            return 'Ets Tercera Divisió';
-        }
-        else{
-            return 'Per reprovar';
-        }
+        return $nota >= 5;
     }
 }
+
 
 ?>
--- tests/calificacionesTest.php
+++ tests/calificacionesTest.php
@@ -1,42 +1,39 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
-
-require_once __DIR__ . '/../src/calificaciones.php';
+use App\Calificaciones;
 
 class CalificacionesTest extends TestCase{
 
     private $calificacion;
 
     protected function setUp(): void{
-        $this->calificacion = new Calificaciones();
+
     }
 
     protected function tearDown(): void{
         $this->calificacion = null;
     }
-    public function testVerificarPrimeraDivision(){
-        $calificacion = new Calificaciones();
-        $res = $calificacion->Calificacion(0.60);      
-        $this->assertEquals('Ets Primera Divisió', $res);
+    public function testEsAprobadoSufi(){
+        $calificacion = new Calificaciones(5);
+        $this->assertTrue($calificacion->Aprobado(5, 10));
     }
 
-    public function testVerificarSegundaDivision(){
-        $calificacion = new Calificaciones();
-        $res = $calificacion->Calificacion(0.45);
-        $this->assertEquals('Ets Segona Divisió', $res);
+    public function testEsAprobadoNotable(){
+        $calificacion = new Calificaciones(7);
+        $this->assertTrue($calificacion->Aprobado(7, 10));
     }
-
-    public function testVerificarTerceraDivision(){
-        $calificacion = new Calificaciones();
-        $res = $calificacion->Calificacion(0.33);
-        $this->assertEquals('Ets Tercera Divisió', $res);
+    public function testEsAprobadoNotaMaxima(){
+        $calificacion = new Calificaciones(10);
+        $this->assertTrue($calificacion->Aprobado(10, 10));
     }
-
-    public function testVerificarPerReprovar(){
-        $calificacion = new Calificaciones();
-        $res = $calificacion->Calificacion(0.32);
-        $this->assertEquals('Per reprovar', $res);
+    public function testEsSuspendido(){
+        $calificacion = new Calificaciones(4);
+        $this->assertFalse($calificacion->Aprobado(4.9, 10));
+    }
+    public function testEsInvalido(){
+        $calificacion = new Calificaciones(-1);
+        $this->assertFalse($calificacion->Aprobado(-1, 10));
     }
 }
 ?>
