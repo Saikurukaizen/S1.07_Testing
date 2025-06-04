96f64a72f285272d383a57a7dba47580b7ffabb6
Marc Sanchez <163992267+Saikurukaizen@users.noreply.github.com>
2025-06-04 20:30:33 +0200

    External Data Provider

--- Lv.2/tests/RankingNotasTest.php
+++ Lv.2/tests/RankingNotasTest.php
@@ -8,17 +8,6 @@ use PHPUnit\Framework\TestCase;
 
 class RankingNotasTest extends TestCase{
 
-    /* private ?RankingNotas $ranking; */
-
-    /* protected function setUp(): void{
-        $this->ranking = new RankingNotas(1.00);
-    }
-
-    protected function tearDown(): void{
-        $this->ranking = null;
-    }
- */
-
     #[DataProvider('RankingNotasDataProviderTest::provideRanking')]
 
     public function testRanking(
@@ -33,24 +22,6 @@ class RankingNotasTest extends TestCase{
             $this->assertSame($terceraDivision, $ranking->checkTerceraDivision());
             $this->assertSame($reprobar, $ranking->checkParaReprobar());
         }
-   /*  public function testCheckPrimeraDivision(): void{
-            $this->ranking = new RankingNotas(0.60);
-        $this->assertTrue($this->ranking->checkPrimeraDivision());   
-    }
-
-    public function testCheckSegundaDivision(): void{
-        $this->ranking = new RankingNotas(0.45);
-        $this->assertTrue($this->ranking->checkSegundaDivision());
-    }
-
-    public function testCheckTerceraDivision(): void{
-        $this->ranking = new RankingNotas(0.33);
-        $this->assertTrue($this->ranking->checkTerceraDivision());
-    }
-
-    public function testCheckParaReprobar(): void{
-        $this->ranking = new RankingNotas(0.32);
-        $this->assertTrue($this->ranking->checkParaReprobar());
-    } */
+  
 }
 ?>
