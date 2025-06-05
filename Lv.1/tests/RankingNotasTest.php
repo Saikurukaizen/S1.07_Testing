3b76aa41769f2b6d6857b649427ed4e77b8bfc84
Marc Sanchez <163992267+Saikurukaizen@users.noreply.github.com>
2025-06-05 10:24:45 +0200

    expanding assertions

--- Lv.1/tests/RankingNotasTest.php
+++ Lv.1/tests/RankingNotasTest.php
@@ -9,31 +9,35 @@ class RankingNotasTest extends TestCase{
 
     private ?RankingNotas $ranking;
 
-    /* protected function setUp(): void{
-        $this->ranking = new RankingNotas(1.00);
-    }
-
-    protected function tearDown(): void{
-        $this->ranking = null;
-    } */
-
     public function testCheckPrimeraDivision(): void{
-            $this->ranking = new RankingNotas(0.60);
-        $this->assertTrue($this->ranking->checkPrimeraDivision());   
+        $this->ranking = new RankingNotas(0.60);
+        $this->assertTrue($this->ranking->checkPrimeraDivision());
+        $this->assertFalse($this->ranking->checkSegundaDivision());
+        $this->assertFalse($this->ranking->checkTerceraDivision());
+        $this->assertFalse($this->ranking->checkParaReprobar());
     }
 
     public function testCheckSegundaDivision(): void{
         $this->ranking = new RankingNotas(0.45);
+        $this->assertFalse($this->ranking->checkPrimeraDivision());
         $this->assertTrue($this->ranking->checkSegundaDivision());
+        $this->assertFalse($this->ranking->checkTerceraDivision());
+        $this->assertFalse($this->ranking->checkParaReprobar());
     }
 
     public function testCheckTerceraDivision(): void{
         $this->ranking = new RankingNotas(0.33);
+        $this->assertFalse($this->ranking->checkPrimeraDivision());
+        $this->assertFalse($this->ranking->checkSegundaDivision());
         $this->assertTrue($this->ranking->checkTerceraDivision());
+        $this->assertFalse($this->ranking->checkParaReprobar());
     }
 
     public function testCheckParaReprobar(): void{
         $this->ranking = new RankingNotas(0.32);
+        $this->assertFalse($this->ranking->checkPrimeraDivision());
+        $this->assertFalse($this->ranking->checkSegundaDivision());
+        $this->assertFalse($this->ranking->checkTerceraDivision());
         $this->assertTrue($this->ranking->checkParaReprobar());
     }
 }
